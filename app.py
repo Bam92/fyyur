f6c087a64c1452e2ab81f8ae3bf24ca181dd9081
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2022-08-24 06:16:56 +0200

    feat: implement search venue feature

--- app.py
+++ app.py
@@ -163,15 +163,10 @@ def search_venues():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for Hop should return "The Musical Hop".
   # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+  query = request.form.get("search_term") 
+  venues = Venue.query.filter(Venue.name.ilike("%" + query + "%")).all()
+  
+  return render_template('pages/search_venues.html', results=venues, count=len(venues), search_term=request.form.get('search_term', ''))
 
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
