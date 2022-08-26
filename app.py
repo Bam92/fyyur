11f90492909ba39201c9b336e125208b694fb477
Abel Lifaefi Mbula <abelmbula@protonmail.com>
2022-08-26 21:59:10 +0200

    feat: (artist) search an artist by name

--- app.py
+++ app.py
@@ -124,6 +124,13 @@ app.jinja_env.filters['datetime'] = format_datetime
 # Controllers.
 #----------------------------------------------------------------------------#
 
+# utility function
+def search():
+  query = request.form.get("search_term") 
+  venues = Venue.query.filter(Venue.name.ilike("%" + query + "%")).all()
+
+
+
 @app.route('/')
 def index():
   return render_template('pages/home.html')
@@ -161,6 +168,7 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
+  # search()
   query = request.form.get("search_term") 
   venues = Venue.query.filter(Venue.name.ilike("%" + query + "%")).all()
   
@@ -169,7 +177,6 @@ def search_venues():
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
-  # TODO: replace with real venue data from the venues table, using venue_id
   venue=Venue.query\
     .join(Area, Venue.area_id == Area.id)\
     .add_columns(Venue.id, Venue.name, Venue.image_link, Area.city, Area.state)\
@@ -253,7 +260,7 @@ def show_venue(venue_id):
     "upcoming_shows_count": 1,
   }
   # data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
-  return render_template('pages/show_venue.html', venue=venues)
+  return render_template('pages/show_venue.html', venue=venue)
 
 #  Create Venue
 #  ----------------------------------------------------------------
@@ -296,8 +303,6 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['POST'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
   try:
     Venue.query.filter(Venue.id == venue_id).delete()
     db.session.commit()
@@ -307,8 +312,6 @@ def delete_venue(venue_id):
   except:
     flash('Unable to delete venue with id = ' + venue_id)
 
-  # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
-  # clicking that button delete it from the db then redirect the user to the homepage
   return render_template('pages/home.html')
 
 
@@ -320,18 +323,21 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
+  query = request.form.get("search_term") 
+  response = Artist.query.filter(Artist.name.ilike("%" + query + "%")).all()
+  print(response)
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 4,
+  #     "name": "Guns N Petals",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
+  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''), count=len(response))
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
